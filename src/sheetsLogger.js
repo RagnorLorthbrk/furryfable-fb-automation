fe1a9c30d7278bc5f09c20b207c8476a708021e6
RagnorLorthbrk <ragnorlrthbrk@gmail.com>
2026-02-17 22:31:21 +0530

    Update sheetsLogger.js

--- src/sheetsLogger.js
+++ src/sheetsLogger.js
@@ -1,1 +1,55 @@
 
+import { google } from "googleapis";
+
+const SHEET_NAME = "Sheet1"; // change if your sheet tab name is different
+
+function getAuth() {
+  return new google.auth.JWT(
+    process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    null,
+    process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    ["https://www.googleapis.com/auth/spreadsheets"]
+  );
+}
+
+export async function getSheetRows() {
+  const auth = getAuth();
+  const sheets = google.sheets({ version: "v4", auth });
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range: `${SHEET_NAME}!A2:M1000`,
+  });
+
+  return response.data.values || [];
+}
+
+export async function appendRow(data) {
+  const auth = getAuth();
+  const sheets = google.sheets({ version: "v4", auth });
+
+  const row = [
+    data.date,
+    data.topic,
+    data.angle,
+    data.postType,
+    data.breed,
+    data.furColor,
+    data.caption,
+    data.hashtags,
+    data.altText,
+    data.imagePrompt,
+    data.imageProvider,
+    data.fbPostId,
+    data.similarityScore,
+  ];
+
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range: `${SHEET_NAME}!A:M`,
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [row],
+    },
+  });
+}
